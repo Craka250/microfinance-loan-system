537600f2c696a94b8abe35d979edb81392e93515
Ayu_bu <otienojobochieng19@gmail.com>
2026-02-07 01:06:28 -0800

    Updated main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,41 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { Toaster } from "react-hot-toast";
+
 import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+import SocketProvider from "./context/SocketContext";
+
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
-      <App />
+      <SocketProvider>
+        <App />
+        <Toaster position="top-right" />
+      </SocketProvider>
     </AuthProvider>
   </BrowserRouter>
 );
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-import App from "./App";
-import "./styles/index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster position="top-right" reverseOrder={false} />
-    </BrowserRouter>
-  </React.StrictMode>
-);
-
-import SocketProvider from "./context/SocketContext";
-
-<BrowserRouter>
-  <AuthProvider>
-    <SocketProvider>
-      <App />
-      <Toaster position="top-right" />
-    </SocketProvider>
-  </AuthProvider>
-</BrowserRouter>
